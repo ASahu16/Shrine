125a8061e065aa7e693676d12856d53f56c38ece
Abhishek Sahu <asahu.m1608@gmail.com>
2022-11-22 01:11:53 +0530

    Add a top app bar, fix #3
    - Add an AppBar widget
    - Add leading menu button
    - Add `search`, `filter` action btn in the app bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,10 +10,47 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            if (kDebugMode) {
+              print('Menu button');
+            }
+          },
+        ),
+        title: const Text('SHRINE'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (kDebugMode) {
+                print('Search button');
+              }
+            },
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              if (kDebugMode) {
+                print('Filter button');
+              }
+            },
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+          ),
+        ],
+      ),
       // TODO: Add a grid view (102)
-      body: Center(
+      body: const Center(
         child: Text('You did it!'),
       ),
       resizeToAvoidBottomInset: false,
