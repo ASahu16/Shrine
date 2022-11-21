f71db96366789bc256929007ed1689561a744ee5
Abhishek Sahu <39129685+ASahu16@users.noreply.github.com>
2022-11-22 02:02:24 +0530

    Add Products card collection,  close #4 (#8)
    * Add `_buildFridCards` function, generates list or cards
    
    * Update `home` page body content
    
    Assign the generated cards to GridView's children in the body
    
    * Show products details in cards

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,66 @@
-import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
+import 'package:shrine/model/product.dart';
+import 'package:shrine/model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+    return products
+        .map((product) => Card(
+              clipBehavior: Clip.antiAlias,
+              // TODO: Adjust card heights (103)
+              child: Column(
+                // TODO: Center items on the card (103)
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset(
+                      product.assetName,
+                      package: product.assetPackage,
+                      // TODO: Adjust the box size (102)
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                      child: Column(
+                        // TODO: Align labels to the bottom and center (103)
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // TODO: Change innermost Column (103)
+                        children: [
+                          // TODO: Handle overflowing labels (103)
+                          Text(
+                            product.name,
+                            style: theme.textTheme.headline6,
+                            maxLines: 1,
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            formatter.format(product.price),
+                            style: theme.textTheme.subtitle2,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ))
+        .toList();
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -18,7 +74,7 @@ class HomePage extends StatelessWidget {
             semanticLabel: 'menu',
           ),
           onPressed: () {
-            if (kDebugMode) {
+            if (foundation.kDebugMode) {
               print('Menu button');
             }
           },
@@ -27,7 +83,7 @@ class HomePage extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              if (kDebugMode) {
+              if (foundation.kDebugMode) {
                 print('Search button');
               }
             },
@@ -38,7 +94,7 @@ class HomePage extends StatelessWidget {
           ),
           IconButton(
             onPressed: () {
-              if (kDebugMode) {
+              if (foundation.kDebugMode) {
                 print('Filter button');
               }
             },
@@ -49,9 +105,11 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      // TODO: Add a grid view (102)
-      body: const Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context), // replace
       ),
       resizeToAvoidBottomInset: false,
     );
